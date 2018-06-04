49c781d5a7a2f9393369c1d511283b166aed0f2e
Pawel <31793531+PriestOfAdanos@users.noreply.github.com>
2018-06-04 08:10:57 +0200

    Add files via upload

--- picky_eater/interactions/views.py
+++ picky_eater/interactions/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+
+@login_required
+@require_http_methods(['POST', 'GET'])
+def my_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        pass
+    else:
+        return render(request, template_name='user_profile.html', context={'user': current_user})
+
+@login_required()
+def home(request):
+    return render(request, template_name='home.html')
+
+
+
